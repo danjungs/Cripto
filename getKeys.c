194fc53d864bcc598a464beab1b4e37a3b5fb7a4
Daniel <danieljungstedt@gmail.com>
2018-07-11 21:42:05 -0300

    melhor manuseio do ponteiro

--- getKeys.c
+++ getKeys.c
@@ -3,12 +3,23 @@
 #include <stdlib.h>
 #define MAX 20
 
+long getnumber(char*,int);
+
 int main(){
 	char *buffer =malloc(sizeof(long));
 	fgets (buffer, MAX, stdin);
-	long cp1 = atoi(buffer++);
-	long cp2 = atoi(buffer);
+	long cp1 = getnumber(buffer,1);
+	long cp2 = getnumber(buffer,2);
+	
 	printf("%ld\n%ld\n",cp1,cp2);
-	free(--buffer);
+	free(buffer);
 	return 0;
+}
+
+long getnumber(char*b,int num){
+	if (num==1)
+		return atoi(b);
+	while(*b++ != ' ');
+		;
+	return atoi(b);
 }
