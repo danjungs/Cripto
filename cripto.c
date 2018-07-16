f20b8c714e5011e121df0b1d478c81f680ead7ae
Daniel <danieljungstedt@gmail.com>
2018-07-16 17:01:45 -0300

    Criptografia adicionada, assim como a leitura das entradas e a criação de uma saida binaria.

--- cripto.c
+++ cripto.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
-#define MAXLIN 1000
+#define MAXLIN 100
+#define FIRST 1
+#define SECOND 2
+#define CRIPTO 3
+#define DESCRIP 4
+#define MAXBUF 20
 //int lelinha (char *linha, int max);
 /* acha: imprime linhas combinando com padrao do primeiro arg. */
 
-int criptoText();
-int descriptoText();
+int criptoText(long,long);
+int descriptoText(long,long);
+void getKeys(long*,long*,int);
 
 int main(int argc, char *argv[]){
 	
 	int c,status=0;
+   long CH1,CH2;
 
 	while (--argc > 0 && (*++argv)[0] == '-')
 		c = *++argv[0];
 		switch (c) {
-			case 'c':
-				status=criptoText();
+			case 'c':            
+            getKeys(&CH1,&CH2,CRIPTO);
+				status=criptoText(CH1,CH2);
 				break;
 			case 'd':
-				status=descriptoText();
+            getKeys(&CH1,&CH2,DESCRIP);
+				status=descriptoText(CH1,CH2);
 				break;
 
 			default:
@@ -34,30 +44,65 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
-int lelinha (char[], int);
-void writeFile(char[]);
+long getnumber(char*,int);
 
-int criptoText(){
+void getKeys(long*ch1,long*ch2,int sentido){
+	char *buffer =malloc(sizeof(long));
+   FILE *fp;
+   char folder[MAXBUF];   
+   if(sentido == CRIPTO)
+      strcpy(folder,"numcripto.txt");
+   else
+      strcpy(folder,"numdescripto.txt");
+ 
+   fp = fopen(folder, "r");
+	fgets (buffer, MAXBUF, fp);
+	*ch1 = getnumber(buffer,FIRST);
+	*ch2 = getnumber(buffer,SECOND);
+	
+	free(buffer);
+}
+
+long getnumber(char*b,int num){
+	if (num==1)
+		return atoi(b);
+	while(*b++ != ' ');
+		;
+	return atoi(b);
+}
+/* ------------------------------------------------------------*/
+/* -----Protótipos de Funções para cripto e descripto----------*/
+int lelinha (char[], int);
+void criptaLine(char *,long,long, long*);
+void writeFile(long*);
+
+/* ------------------------------------------------------------*/
+/* ------Criptografa o texto inteiro linha por linha ----------*/
+int criptoText(long CP1,long CP2){
 	char buffer[MAXLIN];
+   long  *lineCrip = malloc(MAXLIN*sizeof(int));
 	while (lelinha(buffer, MAXLIN)) {
-		for(int i=0; buffer[i] != '\0';i++){
-			if(isalpha(buffer[i]))
-				buffer[i] = buffer[i] +1;
-		}
-		writeFile(buffer);
+      criptaLine(buffer,CP1,CP2,lineCrip);		
+		writeFile(lineCrip);
+		for(int i = 0; lineCrip[i] !='\0';i++)
+      	printf("Letra: %c cripto: %ld\n",buffer[i],lineCrip[i]);
 	}
+	free(lineCrip);
 	return 1;
 }
 
-int descriptoText(){
+int descriptoText(long DP1,long DP2){
 	char buffer[MAXLIN];
+   long  *lineDescrip = malloc(MAXLIN*sizeof(int));
 	while (lelinha(buffer, MAXLIN)) {
 		for(int i=0; buffer[i] != '\0';i++){
 			if(isalpha(buffer[i]))
 				buffer[i] = buffer[i] -1;
 		}
-		writeFile(buffer);
+      criptaLine(buffer,DP1,DP2,lineDescrip);  // SÓ PRA COMPILAR
+		writeFile(lineDescrip);
 	}
+	free(lineDescrip);
 	return 1;
 }
 
@@ -66,15 +111,34 @@ int lelinha (char s [], int lim){
 	int c, i;
 	for(i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
 		s[i] = c;
-
+	if (c == '\n')
+	 	s[i++] = c;
 	s[i]= '\0';
 	return i;
 	}
 	
-void writeFile(char t[]){      
+long eleva(long a,int b){
+	long aux=a;
+	while(b-->1)
+		a=a*aux;
+	return a;
+}
+
+void criptaLine(char *linha,long CP1,long CP2, long*b)
+ {
+ 	int i=0;
+ 	for(i=0; linha[i] != '\0';i++){
+ 		b[i]= (long)linha[i];
+ 		b[i]=  eleva(b[i],CP1) % CP2;
+ 	}
+   b[i] = '\0';
+ }
+
+void writeFile(long *t){      
    FILE *fp;
-   fp = fopen("saida.txt", "a");
-   fprintf(fp,"%s\n",t);
+   fp = fopen("saida.crp", "a");
+   for(int i = 0; t[i] !='\0';i++)
+      fputc(t[i],fp);
    fclose(fp);          
 }	
 	
